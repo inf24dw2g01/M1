53231555fd359fc189baa90d5c9d335e9630e550
Pedro Venda <155490985+PedroVenda27@users.noreply.github.com>
2025-04-23 18:46:31 +0100

    Second Commit

--- index.js
+++ index.js
@@ -8,6 +8,3 @@ app.use('/products', require('./routes/productsRoute'));
 
 
 app.listen(3000, () => console.log('Server is running on port 3000'))
-
-////33.57
-// https://www.youtube.com/watch?v=0pez9kbIoJQ&list=PLSYOk6lU2UL_1NWLD3JidUv2isHTfS0zP
