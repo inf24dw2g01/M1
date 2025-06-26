c6ba7538389d341956a2f0995c779bb2768d1b15
Ephraim Gibson <mrgibs97@gmail.com>
2025-06-26 14:44:10 +0100

    refactored folders

--- backend_M1/src/config/passport.js
+++ backend_M1/src/config/passport.js
@@ -1,4 +1,4 @@
-const passport = require('passport');
+const passport = require('backend_M1/src/config/passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/User');
 
